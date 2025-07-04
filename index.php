73c909c295af3e9ba7551c07b2b8ae3c24bd4cbc
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-04 14:03:55 -0300

    Utilizando strings e funções no PHP

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Escreva seu nome: \n";
+$nome = readline();
+
+echo "Informe sua idade: \n";
+$idade = intval(readline());
+
+echo "Informe sua profissão: \n";
+$profissao = readline();
+
+echo "Olá $nome. Você tem $idade anos. Sua profissão é $profissao. \n";
