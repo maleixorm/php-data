39d1c1b2505f3b760a7471d0550244b00f05bc37
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-04 15:26:05 -0300

    Criando a index

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<?php
-
-echo "Escreva seu nome: \n";
-$nome = readline();
-
-echo "Informe sua idade: \n";
-$idade = intval(readline());
-
-echo "Informe sua profissão: \n";
-$profissao = readline();
-
-echo "Olá $nome. Você tem $idade anos. Sua profissão é $profissao. \n";
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <?php require_once('menu.php'); ?>
+        </div>
+    </main>
+    <footer class="rodape">
+        Marcos Aleixo © <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
