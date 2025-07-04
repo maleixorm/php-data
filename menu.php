deaf8a44aa31a4ebfa1f8313f04d1acaf159b21b
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-04 15:26:20 -0300

    criando o menu personalizado

---
+++ menu.php
@@ -0,0 +1,12 @@
+<nav class="modulos">
+    <div class="modulo verde">
+        <h3>1. Strings</h3>
+        <ul>
+            <li>
+                <a href="exercicio.php?dir=strings&file=tratamento">
+                    Tratando Strings
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
